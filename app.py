d7ccfeb414e2dca0ec43dd5a56a39720d5109c82
manan-2506 <mahajanmanan25@gmail.com>
2026-05-21 21:09:03 +0530

    changed some CORS code

--- app.py
+++ app.py
@@ -74,14 +74,7 @@ def create_flask_app():
 
     app = Flask(__name__, template_folder='templates', static_folder='static')
     
-    # Add CORS headers for web compatibility
-    @app.after_request
-    def add_cors_headers(response):
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        return response
-    
+
     # Global session
     session_cache = {'session': None}
 
